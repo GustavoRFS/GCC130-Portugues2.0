2b6e73c4709c5d8db30faea18234d2bb0d4bca2d
Gustavo Ribeiro da Fonseca Santos <gustavoribeiro.ribeiro1@gmail.com>
2022-07-29 20:14:19 -0300

    Add arg usage to ExemploLexer and test of vouf

--- src/ExemploLexer.java
+++ src/ExemploLexer.java
@@ -1,15 +1,18 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
-
 import java.io.IOException;
 
 public class ExemploLexer {
 
     public static void  main (String[] args){
-        String filename = "/home/felipe/Área de Trabalho/GCC130-Portugues2.0/errado3.txt";
+        if (args.length == 0) {
+            throw new RuntimeException("Arquivo não informado");
+        }
+        String fileName = args[0];
+        String filePath = System.getProperty("user.dir")+"/"+fileName;
         try{
-            CharStream input = CharStreams.fromFileName(filename);
+            CharStream input = CharStreams.fromFileName(filePath);
             Portugues2Lexer lexer = new Portugues2Lexer(input);
             Token token;
             while (!lexer._hitEOF){
